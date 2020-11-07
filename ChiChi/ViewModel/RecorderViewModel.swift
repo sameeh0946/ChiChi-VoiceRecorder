13385d3f70a991bff9d9eb9c4ba90d3d600cff72
sam <abd.sam0946@gmail.com>
2020-11-07 16:53:22 +0530

    Helper functions for Recorder View

--- ChiChi/ViewModel/RecorderViewModel.swift
+++ ChiChi/ViewModel/RecorderViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import AVFoundation
+
+
+final class RecorderViewModel {
+    
+    let settings = [AVFormatIDKey: kAudioFormatLinearPCM, AVLinearPCMBitDepthKey: 16, AVLinearPCMIsFloatKey: true, AVSampleRateKey: Float64(44100), AVNumberOfChannelsKey: 1] as [String : Any]
+    
+    func format() -> AVAudioFormat? {
+        let format = AVAudioFormat(settings: self.settings)
+        return format
+    }
+    
+    func convertFromAVAudioSessionCategory(_ input: AVAudioSession.Category) -> String {
+        return input.rawValue
+    }
+    
+
+    
+
+}
