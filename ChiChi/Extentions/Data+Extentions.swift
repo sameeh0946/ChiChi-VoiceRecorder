d69584f8f9730e61c45e68f82522672cb6d6111a
sam <abd.sam0946@gmail.com>
2020-11-07 16:04:49 +0530

    Data extols to convert audio into pcm chunks buffer

---
+++ ChiChi/Extentions/Data+Extentions.swift
@@ -0,0 +1,31 @@
+//
+//  Data+Extentions.swift
+//  ChiChi
+//
+//  Created by Rishabh Karotiya on 07/11/20.
+//
+
+import Foundation
+
+extension Data {
+    init(buffer: AVAudioPCMBuffer) {
+        let audioBuffer = buffer.audioBufferList.pointee.mBuffers
+        self.init(bytes: audioBuffer.mData!, count: Int(audioBuffer.mDataByteSize))
+    }
+    
+    func toPCMBuffer(format: AVAudioFormat) -> AVAudioPCMBuffer? {
+        let streamDesc = format.streamDescription.pointee
+        let frameCapacity = UInt32(count) / streamDesc.mBytesPerFrame
+        guard let buffer = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: frameCapacity) else { return nil }
+        
+        buffer.frameLength = buffer.frameCapacity
+        let audioBuffer = buffer.audioBufferList.pointee.mBuffers
+        
+        withUnsafeBytes { (bufferPointer) in
+            guard let addr = bufferPointer.baseAddress else { return }
+            audioBuffer.mData?.copyMemory(from: addr, byteCount: Int(audioBuffer.mDataByteSize))
+        }
+        
+        return buffer
+    }
+}
